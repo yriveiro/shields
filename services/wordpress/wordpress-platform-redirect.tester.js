7cc93536841038e652b7c8dba247e6b834bec694
Paul Melnikow <email@paulmelnikow.com>
2020-02-09 23:52:17 -0500

    [DEPLOY] Completely clean the index

--- services/wordpress/wordpress-platform-redirect.tester.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const t = (module.exports = require('../tester').createServiceTester())
-
-t.create('Plugin Tested WP Version (Alias)')
-  .get('/akismet.svg')
-  .expectRedirect('/wordpress/plugin/tested/akismet.svg')
